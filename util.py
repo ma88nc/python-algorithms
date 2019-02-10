b1acf59d80f15b9e614edaa89015facab4cd9c9b
Mario Annibaldi <mannibaldi@mail.com>
2019-02-10 09:32:47 -0500

    Add randomizeList function

--- util.py
+++ util.py
@@ -8,5 +8,22 @@ def randomList(minValue, maxValue, size):
 
     return lst
 
+# Randomize list using the Fisher-Yates shuffle algorithm.
+def randomizeList(lst):
+    # Read array from highest index to lowest
+    sz = len(lst)
+    for i in range(sz-1, 0, -1):
+        # Generate a random number j such that 0 <= j <= i
+        j = randint(0, i)
 
+        # Swap current element with random generated index
+        temp = lst[j]
+        lst[j] = lst[i]
+        lst[i] = temp
+
+    return lst
+
+# Swap two variables
+def swap(x, y):
+    return y, x    
     
