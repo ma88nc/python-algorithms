42671eeea16405e59df95de5fe2d60b904da8f64
Mario Annibaldi <mannibaldi@mail.com>
2019-03-18 05:32:07 -0400

    Implemented "add sorted" feature

--- linked-list.py
+++ linked-list.py
@@ -44,6 +44,41 @@ class LinkedList:
                 current = current.next    
             current.next = toAdd 
 
+    def addSorted(self, data):
+        print("Here in addSorted with {}".format(data))
+        if self.head == None:
+            print("Adding to head "+data)
+            newNode = Node()
+            newNode.next = None
+            newNode.data = data
+            self.head = newNode
+        else:
+            current = self.head
+            while current != None:
+                print(" - current.data is "+ current.data)
+                if data < current.data:
+                    print("Adding {} to middle before current.data {}".format(data, current.data))
+                    newNode = Node()
+                    newNode.data = data
+                    newNode.next = current
+                    if current == self.head:
+                        print("   Adding to head")
+                        self.head = newNode
+                    else:
+                        print("   Adding in middle")
+                        #current = newNode     
+                        prev.next = newNode                  
+                    return
+                prev = current    
+                current = current.next
+            if current == None:
+                print("Adding to end "+ data)
+                newNode = Node()
+                newNode.data = data
+                print("prev.data is "+ prev.data)
+                prev.next = newNode
+                newNode.next = None 
+
     def deleteItem(self, data):
         prev = None
         current = self.head
@@ -67,6 +102,9 @@ def main():
     myList1.addFirst("Ron")
     myList1.addFirst("Hermione")
     myList1.addFirst("Ginny")
+    myList1.addFirst("Parvati")
+    myList1.addFirst("Seamus")
+    myList1.addFirst("Neville")
     myList1.printAllNodes()
 
     # Delete an item from the list
@@ -82,6 +120,30 @@ def main():
     myList2.addLast("Hagrid")
     myList2.addLast("McGonagall")
     myList2.printAllNodes()
+
+    myList3 = LinkedList()
+    print("\nLinkus listus addus sortus")
+
+    myList3.addSorted("Harry")
+    myList3.printAllNodes()
+    myList3.addSorted("Ginny")
+    myList3.printAllNodes()
+    myList3.addSorted("Hermione")
+    myList3.printAllNodes()
+    myList3.addSorted("Ron")
+    myList3.printAllNodes()
+    myList3.addSorted("Parvati")
+    myList3.printAllNodes()
+    myList3.addSorted("Seamus")
+    myList3.printAllNodes()
+    myList3.addSorted("Neville")
+    myList3.printAllNodes()
+    myList3.addSorted("Katie")
+    myList3.printAllNodes()
+    myList3.addSorted("George")
+    myList3.printAllNodes()
+    myList3.addSorted("Fred")
+    myList3.printAllNodes()
 
 if __name__ == '__main__':
     main()        
